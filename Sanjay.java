37944b994e28d474d5b03923cefed0de7d10c2d9
AbdullahErindale <867292@pdsb.net>
2024-09-19 12:30:40 -0400

    Create Sanjay.java

---
+++ Sanjay.java
@@ -0,0 +1,12 @@
+class Sanjay {
+  public static void main(String [] args) {
+    // place your code here!
+    int bowl = 6;
+    int cookies = 144;
+    int dist = cookies / bowl;
+    System.out.println("Welcome to Sanjay's Koala Party!");
+    System.out.println("Sanjay has " + bowl +  " bowls for " + cookies +  " cookies");
+    System.out.println("The number of cookies per bowl is: " + dist);
+    // your code ends here!
+  }
+}
