22c1dcec0d6f1e21798c259f8f655cc66ac59ed3
AbdullahErindale <867292@pdsb.net>
2024-09-19 12:12:56 -0400

    Create Mathtest1.java

---
+++ Mathtest1.java
@@ -0,0 +1,10 @@
+class Mathtest1 {
+  public static void main(String [] args) {
+    // place your code here!
+    double circ;
+    double rad = 3.0;
+    circ = 2 * Math.PI * rad;
+    System.out.println("The circumference of a circle of radius " + rad + " is " + circ);
+    // your code ends here!
+  }
+}
