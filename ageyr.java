420767be3464175322f9c63f545302c332eab0ab
AbdullahErindale <867292@pdsb.net>
2024-09-20 14:17:48 -0400

    Create ageyr.java

---
+++ ageyr.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+class ageyr {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int age;
+        int yr;
+        System.out.print("What is your age: ");
+        age = scan.nextInt();
+        System.out.print("What year were you born: ");
+        yr = scan.nextInt();
+        
+        int yr2 = yr * 2 + 5;
+        System.out.println(yr2);
+        int yr3 = yr2 * 50 + age;
+        System.out.println(yr3);
+        int yr4 = yr3 - 250;
+        System.out.println(yr4);
+        int yr5 = yr4 / 100;
+        
+        System.out.print(yr5);
+    }
+}
