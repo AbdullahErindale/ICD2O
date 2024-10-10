39c38245e7424425c6e9da24d7a58f5c7152cbc3
AbdullahErindale <867292@pdsb.net>
2024-10-10 13:40:30 -0400

    Create lesson4.java

---
+++ lesson4.java
@@ -0,0 +1,13 @@
+class lesson4 {
+  public static void main(String [] args) {
+    // place your code here!
+    int amount = 8;
+    amount = amount + 1;
+    System.out.println(amount);
+  
+    double a = 8.0;
+    int b = 3;
+    System.out.println(a/b);
+    // your code ends here!
+  }
+}
