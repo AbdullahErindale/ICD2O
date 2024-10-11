00aca4758ee5764f46626281ba6ca25e66898e44
AbdullahErindale <867292@pdsb.net>
2024-10-10 21:01:45 -0400

    Update assignment1.java

--- assignment1.java
+++ assignment1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package assignment1;
+
 import java.util.Scanner;
 import java.lang.Math;
 /**
@@ -23,60 +23,60 @@ public class assignment1 {
         //tells use what to do
         
         System.out.print("Length: ");
-        int L = scan.nextInt();
+        double L = scan.nextInt();
         //asks user the length and assigns it to var L
         
         System.out.print("Width: ");
-        int W = scan.nextInt();
+        double W = scan.nextInt();
         //asks user the width and assigns it to var W
         
-        int arearec = L * W;
+        double arearec = L * W;
         //does formula for the area of a rectangle
         
-        System.out.println("Area of the rectangle is " + arearec);
+        System.out.printf("Area of the rectangle is %.2f\n", arearec);
         //prints the area of the rectangle
         
         System.out.println("Enter the radius of a circle and I will calculate the area");
         //tells use what to do
         
         System.out.print("Radius: ");
-        int R = scan.nextInt();
+        double R = scan.nextInt();
         //asks user the radiues and assigns it to the var R
         
         
-        int r = R * R;
+        double r = R * R;
         //does r^2 
         
         double areacirc = Math.PI * r;
         //does the rest of the formula
         
-        System.out.println("Area of the circe is " + areacirc);
+        System.out.printf("Area of the circe is %.2f\n", areacirc);
         //prints out the area of the circle out on the console
         
         System.out.println("Enter the height and raidius of a cylinder and I will calculate the surface"
                 + " area and volume");
         
         System.out.print("Radius: ");
-        int R2 =  scan.nextInt();
+        double R2 =  scan.nextInt();
         //asks for the radies and assigns it to R2
         
         System.out.print("Height: ");
-        int H = scan.nextInt();
+        double H = scan.nextInt();
         //asks for the height and assigns it to H
         
-        int r2 = R2 * R2;
+        double r2 = R2 * R2;
         //does r^2
         
         double V = Math.PI * r2 * H;
         //does the rest of the formula for volume
         
-        System.out.println("Volume of the cylinder is " + V);
+        System.out.printf("Volume of the cylinder is %.2f\n", V);
         //prints out the valume of the cylinder on the console
         
         double SA = 2 * Math.PI * R2 * H + 2 * Math.PI * r2;
         //does the formula for the surface area
         
-        System.out.println("Surface area of the cylinder is " + SA);
+        System.out.printf("Surface area of the cylinder is %.2f\n", SA);
         //prints out the surface area of the cyclinder out on the console
         
         System.out.println("Enter the length of a pendulum and I will calculate the period");
@@ -85,7 +85,7 @@ public class assignment1 {
         //value of gravity
         
         System.out.print("Length: ");
-        int L2 = scan.nextInt();
+        double L2 = scan.nextInt();
         //asks what the length is and assigns it to L2
 
         double L3 = L2 / g;
@@ -97,7 +97,7 @@ public class assignment1 {
         double P = 2 * Math.PI * L4;
         //does formula for period
         
-        System.out.println("Period of the pendulum is " + P);
+        System.out.printf("Period of the pendulum is %.2f\n", P);
     }
     
 }
