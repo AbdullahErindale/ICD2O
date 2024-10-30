78a66f7c5e305041af21be2e6f3d888acd8ad1ea
AbdullahErindale <867292@pdsb.net>
2024-10-30 13:56:38 -0400

    Update U5 L7.java

--- U5 L7.java
+++ U5 L7.java
@@ -4,25 +4,19 @@ public class U5 L7 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in); //open for keyboard input
         System.out.print("Enter an integer: ");
-
+        double a = kb.nextInt();
+        System.out.print("Enter an integer: ");
+        double b = kb.nextInt();
         try {
-            double a = kb.nextInt();
             System.out.println("You entered: " + a);
-            System.out.print("Enter an integer: ");
-            double b = kb.nextInt();
             System.out.println("You entered: " + b);
-            if (b == 0) {
-              System.out.println("Cannot divide by 0");
-            }
-            else {
-              double c = a/b;
-            System.out.println("Num 1/Num 2 = " + c);
-            }
+            double c = a/b;
+            System.out.println(c);
+          
             
-        } catch (Exception e) {
+        } catch (ArithmeticException e) {
             System.out.println("Invalid input. Please enter a valid integer.");
-        }  
-
+          }
         
         
         kb.close();
