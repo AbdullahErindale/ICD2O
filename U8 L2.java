fec3fdd71bc6a0c09943b3e02a02364e8103c9b0
AbdullahErindale <867292@pdsb.net>
2024-11-05 12:11:34 -0500

    Create U8 L2.java

---
+++ U8 L2.java
@@ -0,0 +1,18 @@
+// any import statements go here!
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        // Your code goes here!
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give me 2 numbers");
+        System.out.print("Number 1:");
+        double num1 = scan.nextInt();
+        System.out.print("Number 2");
+        double num2 = scan.nextInt();
+        double num3 = num1 + num2;
+        System.out.println("The product of the 2 numbers is " + num3);
+        // Yout code stops here!
+    }
+}
