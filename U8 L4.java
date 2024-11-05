ae167429a328cbdbd31399d12819375d3ef985f5
AbdullahErindale <867292@pdsb.net>
2024-11-05 13:04:30 -0500

    Create U8 L4.java

---
+++ U8 L4.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
+class U8 L4 {
+  public static void main(String [] args) {
+    Scanner kb = new Scanner(System.in);
+    String choice = "0";
+    String a = "a";
+    String b = "b";
+    boolean valid = false;
+    while (! valid) {
+        System.out.print("Choose a, b or c: ");
+        choice = kb.nextLine();
+        valid = ((choice == a) || (choice == b));
+        if (valid = true) {
+          System.out.println("You chose: " + choice);
+        }
+    }
+  }
+}
