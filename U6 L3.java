c78d676982057c7b2ec1212490d0ac6e0aa40554
AbdullahErindale <867292@pdsb.net>
2024-11-07 13:05:52 -0500

    Create U6 L3.java

---
+++ U6 L3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class U6 L3 {
+   public static void main(String [] args) {
+      // enter your code here!
+      Scanner kb = new Scanner(System.in);
+      
+      System.out.print("Enter a percentage mark (0 - 100): ");
+      int grade = kb.nextInt();
+      
+      if ((grade >= 0) && (grade <= 49)) {
+        System.out.println(grade + " is a letter grade of F");
+      } else if ((grade >= 50) && (grade <= 59)) {
+        System.out.println(grade + " is a letter grade of D");
+      } else if ((grade >= 60) && (grade <= 69)) {
+        System.out.println(grade + " is a letter grade of C");
+      } else if ((grade >= 70) && (grade <= 79)) {
+        System.out.println(grade + " is a letter grade of B");
+      } else if ((grade >= 80) && (grade <= 100)) {
+        System.out.println(grade + " is a letter grade of A");
+      }
+   }
+}
