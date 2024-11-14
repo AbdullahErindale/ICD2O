d90bda114dea29f4721dfa2025e2538133aeb7dc
AbdullahErindale <867292@pdsb.net>
2024-11-14 14:23:52 -0500

    Create U6 L5.java

---
+++ U6 L5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class factorial {
+   public static void main(String [] args) {
+      Scanner kb = new Scanner(System.in);
+      System.out.print("Enter a number: ");
+      int n = kb.nextInt();
+      int fac = 1;
+      while (n <= 4) {
+          fac = fac * n;
+          n++;
+      }
+      System.out.println("Answer = " + fac);
+   }
+}
