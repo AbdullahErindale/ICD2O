d22615c19e1d41dc75aab3d126b8f4f6ad5812c2
AbdullahErindale <867292@pdsb.net>
2024-11-15 12:29:51 -0500

    Update and rename U6 L7.java to U6_L7.java

--- U6_L7.java
+++ U6_L7.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
 
-class U6 L7 {
+class U6_L7 {
    public static void main(String [] args) {
       Scanner kb = new Scanner(System.in);
+      Random r = new Random();
       System.out.print("Enter a number: ");
       boolean valid = false;
-      int answer = 5;
+      int answer = r.nextInt(10)+1;
       while (! valid) {
           System.out.print("Guess a number between 1 - 10: ");
           int guess = kb.nextInt();
@@ -13,6 +15,9 @@ class U6 L7 {
             if (guess == answer) {
               valid = true;
             }
+          }
+          if((guess < 1) || (guess > 10)) {
+            System.out.println("Incorrect, try again");
           }
       }
       System.out.println("Correct");
