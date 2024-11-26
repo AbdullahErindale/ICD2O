8fb859253bcfe4c830316ec6b9a3e25c83d7c69b
AbdullahErindale <867292@pdsb.net>
2024-11-26 14:25:51 -0500

    Update collatz.java

--- collatz.java
+++ collatz.java
@@ -1,25 +1,45 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
 import java.util.Scanner;
-class Main {
+/**
+ *
+ * @author 867292
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        // TODO code application logic here
         Scanner kb = new Scanner(System.in);
-        int n = 0;
+        long n = 2;
+        long amount = 0;
+        long largest = 0;
         System.out.println(n);
         
-        while (n <= 1) {
-            System.out.print("Enter a value for n: ");
+        while (n >= 1) {
+            System.out.print("\nEnter a value for n: ");
              n = kb.nextInt();
             while (n > 1) {
                 if ( (n % 2) == 0 ) {
                     n = n / 2;
-                    System.out.println(n);
+                    System.out.print(n + "; ");
                 }
                 else {
                     n = 3 * n + 1;
-                    System.out.println(n);
+                    System.out.print(n + "; ");
+                }
+                if (n > largest) {
+                    largest = n;
                 }
             }
+            
         }
     }
+    
 }
