f237652568ffdd47e146d518baf8659af8a0bb69
AbdullahErindale <867292@pdsb.net>
2024-11-27 13:06:12 -0500

    Update collatz.java

--- collatz.java
+++ collatz.java
@@ -17,29 +17,35 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner kb = new Scanner(System.in);
-        long n = 2;
-        long amount = 0;
+        long n = 0;
+        long amount;
         long largest = 0;
-        System.out.println(n);
-        
-        while (n >= 1) {
-            System.out.print("\nEnter a value for n: ");
-             n = kb.nextInt();
+        char resp;
+        do {
+            System.out.print("Enter a value for n: ");
+            n = kb.nextLong();
+            System.out.print(n + "; ");
             while (n > 1) {
+                largest = 0;
                 if ( (n % 2) == 0 ) {
                     n = n / 2;
                     System.out.print(n + "; ");
+                    if (n > largest) {
+                        largest = n;
+                    }
                 }
                 else {
                     n = 3 * n + 1;
                     System.out.print(n + "; ");
-                }
-                if (n > largest) {
-                    largest = n;
+                    if (n > largest) {
+                        largest = n;
+                    }
                 }
             }
+            System.out.println("the largest number was " + largest);
+            System.out.print("continue? (y/n)");
+            resp = kb.next().toLowerCase().charAt(0);
+        } while (resp == 'y');
             
-        }
     }
-    
 }
