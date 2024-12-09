9de82f014a59a726efec05b57356867b910e019b
AbdullahErindale <867292@pdsb.net>
2024-12-09 12:28:36 -0500

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -3,30 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Assignment2;
+package javaapplication2;
 import java.util.Scanner;
 /**
  *
  * @author 867292
  */
-public class Assignment2 {
+public class JavaApplication2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Creates a scanner object
         Scanner kb = new Scanner(System.in);
+        //creates n and other required variables
         long n = 0;
         long amount = 0;
         long largest = 0;
         char resp;
+        //actual collatz conjecture
         do {
+            //resets variable largest to 0
             largest = 0;
+            //Asks for a value to use for n and assigns it to n
             System.out.print("Enter a value for n: ");
             n = kb.nextLong();
             System.out.printf("%,d; ", n);
+            //while loop so that the loop doesn't go on infinitely
             while (n > 1) {
+                //checks if n is even or odd and acts accordingly
                 if ( (n % 2) == 0 ) {
                     n = n / 2;
                     System.out.printf("%,d; ", n);
@@ -35,14 +41,17 @@ public class Assignment2 {
                     n = 3 * n + 1;
                     System.out.printf("%,d; ", n);
                 }
+                //this if statement finds out which number is the largest 
                 if (n > largest) {
                     largest = n;
                 }
+                //adds 1 to amount to count how many numbers are being printed
                 if (n >= 1) {
                 amount++;
                 }
             }
             amount++;
+            //prints largest number and amount of numbers then asks to continue while response is y
             System.out.printf("\nthe largest number was %,d", largest);
             System.out.printf("\nthere were %,d terms printed", amount);
             System.out.print("\ncontinue? (y/n)");
